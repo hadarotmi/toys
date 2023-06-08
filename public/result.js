eb45c4e89f08f02a49a0050ec374551b08656729
hadarotmi <hadarotmi123@gmail.com>
2023-06-08 09:13:53 +0300

    change render port

--- public/result.js
+++ public/result.js
@@ -6,31 +6,31 @@ const init = () => {
     getById()
 }
 const getAllList = async () => {
-    let url = "http://localhost:3002/toys/all";
+    let url = "https://toys-api-18bf.onrender.com/toys/all";
     let resp = await axios.get(url);
     doGetApi(resp,"#allToys")
 }
 
 const getBySearchName = async () => {
-    let url = "http://localhost:3002/toys/search?s=car";
+    let url = "https://toys-api-18bf.onrender.com/toys/search?s=car";
     let resp = await axios.get(url);
     doGetApi(resp,"#shearchName")
 }
 
 const getByPrice = async () => {
-    let url = "http://localhost:3002/toys/price?min=35&max=60";
+    let url = "https://toys-api-18bf.onrender.com/toys/price?min=35&max=60";
     let resp = await axios.get(url);
     doGetApi(resp,"#price")
 }
 
 const getByCaterory = async () => {
-    let url = "http://localhost:3002/toys/category/strategy";
+    let url = "https://toys-api-18bf.onrender.com/toys/category/strategy";
     let resp = await axios.get(url);
     doGetApi(resp,"#category")
 }
 
 const getById = async () => {
-    let url = "http://localhost:3002/toys/single/647c38297bb0d9ed415e2922";
+    let url = "https://toys-api-18bf.onrender.com/toys/single/647c38297bb0d9ed415e2922";
     let resp = await axios.get(url);
     doGetApi(resp,"#singleId")
 }
